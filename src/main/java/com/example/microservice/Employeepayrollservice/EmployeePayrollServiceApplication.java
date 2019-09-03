3251e661de3a36dd33f785cf3f3ea3d86aec87a4
soutam <soutampaul@gmail.com>
2019-09-03 08:42:07 +0530

    Feign template implementation

--- src/main/java/com/example/microservice/Employeepayrollservice/EmployeePayrollServiceApplication.java
+++ src/main/java/com/example/microservice/Employeepayrollservice/EmployeePayrollServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.microservice.Employeepayrollservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("com.example.microservice.Employeepayrollservice")
 public class EmployeePayrollServiceApplication {
 
 	public static void main(String[] args) {
